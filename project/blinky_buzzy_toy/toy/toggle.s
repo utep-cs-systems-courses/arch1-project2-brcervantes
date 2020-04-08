8db4b535770f905996c876b422bf8b6f1d747b26
brcervantes <brcervantes@miners.utep.edu>
2020-04-08 10:22:28 -0600

    assembly update

--- project/blinky_buzzy_toy/toy/toggle.s
+++ project/blinky_buzzy_toy/toy/toggle.s
@@ -1,28 +1,23 @@
-	.file	"state_machine.c"
-.text
-	.balign 2
-	.global	toggle
+	.arch msp430g2553
+	.p2align 1,0	
 	
+	.data
+state:
+	.word 1
+	
+	.text
+jt:
+	.word state1
+	.word state2
+	.word state3
+	.word state4
+	
+	.global	toggle
 toggle:
-	MOV.B	&state, R12 	; Move current state to register 12
-	CMP	#1, R12		; Compare immediate value 1 with register 12
-	JEQ	state2		; If state = 1 jump to state1
 
-	MOV.B	#1, R13		; Move immediate value 1 to register 13
-	CMP	R12, R13	; Compare register 12 with register 13
-	JL	switch		; If less than that means register 12 is greater than 1 jump to switch.
-
-	CMP	#0, R12		; Compare immediate value 0 with current state in register 12
-	JEQ	state1		; Jump to state1 if equal to 0
-	JMP	end		; If it is not equal to zero than jump to end
-
-switch:
-	CMP	#2, R12		; Current state is greater than 1 so we compare to immediate value 2
-	JEQ	state3 		; Jump to state3 if equal to 2
-
-	CMP	#3, R12		; Compare immediate value 3 to current state in register 12
-	JEQ	state4		; Jump to state4 if equal to 3
-	JMP	end		
+	MOV &state, r12
+	ADD r12, r12
+	MOV jt(r12), r0
 
 ;; both leds are off to represent the number 0 in binary
 state1:
@@ -53,8 +48,5 @@ state4:
 	JMP	end
 end:	
 	
-	POP r0			; return to program counter
-	.size	toggle, .-toggle
-	.local  state		; local static variable state
-	.comm	state,1,1
+	POP r0 ; return to program counter
 	
